a6fd4764a9183a2c39599b1fc2c53f03d4eaab09
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-24 09:19:14 -0300

    dgftr6y

---
+++ atv10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a hora (0-23): ");
+        int hora = int.Parse(Console.ReadLine());  // Lê a hora fornecida pelo usuário
+
+        if (hora >= 6 && hora <= 22)
+        {
+            Console.WriteLine("Acesso permitido");
+        }
+        else
+        {
+            Console.WriteLine("Fora do horário");
+        }
+    }
+}
