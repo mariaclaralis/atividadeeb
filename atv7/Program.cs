7dc9f4bd97097628d2669f251b8d02e8544ab691
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-24 09:10:36 -0300

    tyuyutyu

---
+++ atv7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool cadastrado = true;
+        bool cupomValido = false;
+        bool jaUsouCupom = false;
+
+        if (cadastrado && cupomValido && !jaUsouCupom)
+        {
+            Console.WriteLine("Promoção válida");
+        }
+        else
+        {
+            Console.WriteLine("Promoção inválida");
+        }
+    }
+}
