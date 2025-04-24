eda479674d8d00fa0ae0db9e556f032401df4b3b
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-24 08:45:41 -0300

    45tygfh

---
+++ atividadeec/atividadeec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = 25;
+
+        if (numero < 10 || numero > 20)
+        {
+            Console.WriteLine("Fora do intervalo");
+        }
+        else
+        {
+            Console.WriteLine("Dentro do intervalo");
+        }
+    }
+}
