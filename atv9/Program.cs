ceb7ac2a993361499194b08d5da58dc39ffa1bcf
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-24 09:12:31 -0300

    fykuikioulo

---
+++ atv9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int hora = 22;
+
+        if (hora >= 6 && hora <= 22)
+        {
+            Console.WriteLine("Acesso permitido");
+        }
+        else
+        {
+            Console.WriteLine("Fora do horário");
+        }
+    }
+}
