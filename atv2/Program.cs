57703a0011a2f72905db5ed9853f2842ff0b06b1
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-24 08:54:43 -0300

    thyu7y7hn

---
+++ atv2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int idade = 20;
+        bool temCarteira = true;
+
+        if (idade >= 18 && temCarteira)
+        {
+            Console.WriteLine("Pode dirigir");
+        }
+        else
+        {
+            Console.WriteLine("Não pode dirigir");
+        }
+    }
+}
