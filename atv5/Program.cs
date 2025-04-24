e160c5582baf6ed450c38d4dfba5cacd2f07d755
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-24 09:07:30 -0300

    ghfhgbb

---
+++ atv5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ehClienteVip = false;
+        int compras = 12;
+
+        if (!ehClienteVip && compras > 10)
+        {
+            Console.WriteLine("Ganha desconto especial");
+        }
+        else
+        {
+            Console.WriteLine("Não ganha desconto especial");
+        }
+    }
+}
