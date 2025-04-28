e4c17e16f974b2b956ec468f3031327e310c0e1d
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-28 09:07:44 -0300

    jbjugyubbguy7t

---
+++ ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿class program
+{
+    static void Main()
+    {
+        int valor = 1;
+
+        switch (valor) 
+        { 
+        case 1:
+                Console.WriteLine("valor digitado foi 1");
+                break;
+
+        case 2:
+                Console.WriteLine("Valor digitado foi 2");
+                break;
+
+            default:
+                Console.WriteLine("valor não encontrado");
+                break;
+        }
+
+
+
+    }
+}
