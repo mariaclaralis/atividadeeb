8c2ce29f40d10960894dcd9222809ff61b67086b
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-28 09:11:39 -0300

    gndfgffdetr

---
+++ atv1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número de 1 a 7 para o dia da semana: ");
+        int dia = int.Parse(Console.ReadLine());
+
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("Domingo");
+                break;
+            case 2:
+                Console.WriteLine("Segunda-feira");
+                break;
+            case 3:
+                Console.WriteLine("Terça-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 7:
+                Console.WriteLine("Sábado");
+                break;
+            default:
+                Console.WriteLine("Dia inválido!");
+                break;
+        }
+    }
+}
